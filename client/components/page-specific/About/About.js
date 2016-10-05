133b2584d955ef7329ba7a2f92e87016690a802e
tom <tom.yeoman@live.co.uk>
2016-10-05 22:33:04 +0100

    About page added, Re-done shadow boxing

--- client/components/page-specific/About/About.js
+++ client/components/page-specific/About/About.js
@@ -1,5 +1,5 @@
 
-
+import Shadow from '../../css/shadow.css'
 // React
 import React from 'react'
 
@@ -21,7 +21,21 @@ export default class Home extends React.Component {
   render () {
     return (
       <div className={`col-xs-12 row-no-padding`}>
-        Hello
+        <div className="container main-container">
+          <div className="col-xs-12 col-sm-3">
+            <p>Let us help you save money on Critical Illness Cover</p>
+          </div>
+          <div className={`${Shadow.containerShadow} col-xs-12 col-sm-9`}>
+            <div className="col-xs-6">
+                <h3>Are you looking for...</h3>
+                <h4>Critical Illness Cover</h4>
+                <p>This provides a one-off tax free lump sum payment in the event of a serious illness like cancer or permanent disability caused by injury. </p>
+            </div>
+            <div className="col-xs-3">
+              <input type="button" className="btn btn-primary text-center" value="GET QUOTE"/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
