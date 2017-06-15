58e40516641ecc27ad721bd3b1f7438bda2be5a6
Tom Yeoman <tom.yeoman@live.co.uk>
2017-06-15 18:16:28 +0100

    Git copy is now ready to be cloned and deployed

--- server/index_prod.js
+++ server/index_prod.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
-var compression = require('compression');
+// var compression = require('compression');
 
 var app = express();
-app.use(compression());
+// app.use(compression());
 
 app.use('/css',express.static(path.join(__dirname, 'public/css')));
 app.use('/images',express.static(path.join(__dirname, 'public/assets')));
